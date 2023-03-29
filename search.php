aaa25f86f3a22453b461e786daa5f4e33bc6492f
LiamGongola <59128783+LiamGongola@users.noreply.github.com>
2023-03-29 18:06:01 -0400

    clears old pins on search

--- search.php
+++ search.php
@@ -7,7 +7,6 @@
     </head>
     <body>
         <h1>Search all US Craigslist Postings</h1>
-
         <h4>Enter item to scrape for</h4>
         <!--Help Popup code-->
         <div class="popup" style="margin-left: 15px font-family: Arial" onclick="hePop()">Click here for Help
@@ -17,7 +16,6 @@
                 <p style="text-align:left margin-left: 15px">2. Wait</p>
             </span>
         </div>
-
         <div>
             <input id="searchInput" type="text" placeholder="Enter Item">
             <button id="searchSubmitButton">Search</button>
@@ -26,13 +24,20 @@
 	  <br>
 	  
 	  <div id="map"></div>
-
         <!--JS goes here-->
         <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC35EMzaJEToK-ZyOZ1SkBa1yKegKAfb8o&callback=initMap"></script>
 		<script>
 		
 		let global_map;
-
+		let markersArray = [];
+		
+		function clearPins() {
+		  for (var i = 0; i < markersArray.length; i++ ) {
+			markersArray[i].setMap(null);
+		  }
+		  markersArray.length = 0;
+		}
+		
 		$("#searchInput").keyup(function(e) {
 			if(e.key === "Enter"){
 				submitSearch();
@@ -40,6 +45,7 @@
 		});
 		$("#searchSubmitButton").click(submitSearch);
 		function submitSearch(e) {
+			clearPins();
 			const searchText = $("#searchInput").val();
 			console.log("Submitted search:", searchText);
 			$.post("backend/search.php?q=" + searchText, function(data, status) {
@@ -53,16 +59,14 @@
 			var popup = document.getElementById("helpPopup");
 			popup.classList.toggle("show");
 		}
-
 		function initMap() {
 		  global_map = new google.maps.Map(
 			document.getElementById('map'), {zoom: 4, center: {lat: 41.36444, lng: -98.31665}}
 		  );
 		}
-
 		function addMarker(coords, price, title, url){
 		  let marker = new google.maps.Marker({position: coords, map: global_map});
-		  console.log("marker url:" + url);
+		  markersArray.push(marker);
 		  let infoWindow = new google.maps.InfoWindow({
 			  content: `<h3>${title}: ${price}$</h3> \n <a href=${url}>view listing<a>`
 		  });
