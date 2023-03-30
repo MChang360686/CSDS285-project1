eb313bd1668a1ef3f87b75b69157f266e5d35ef4
LiamGongola <59128783+LiamGongola@users.noreply.github.com>
2023-03-30 13:09:48 -0400

    added key +other styles

--- search.php
+++ search.php
@@ -6,24 +6,35 @@
     <link rel="stylesheet" href="style.css" type="text/css">
     </head>
     <body>
-        <h1>Search all US Craigslist Postings</h1>
-        <h4>Enter item to scrape for</h4>
-        <!--Help Popup code-->
-        <div class="popup" style="margin-left: 15px font-family: Arial" onclick="hePop()">Click here for Help
-            <span class="popuptext" id="helpPopup">
-                <p style="text-align:left margin-left: 15px">How to use this application</p>
-                <p style="text-align:left margin-left: 15px">1. Enter name of item you wish to purchase from Ebay into input</p>
-                <p style="text-align:left margin-left: 15px">2. Wait</p>
-            </span>
-        </div>
-        <div>
-            <input id="searchInput" type="text" placeholder="Enter Item">
-            <button id="searchSubmitButton">Search</button>
-        </div>
+	
+	<div id="header">
 		
-	  <br>
-	  
-	  <div id="map"></div>
+		<div id="search">
+			<h1>Search all US Craigslist Postings</h1>
+			<h4 style="margin-bottom: 0;">Enter item to scrape for</h4>
+			 <div style="margin-bottom: 10px;">
+				<input id="searchInput" type="text" placeholder="Enter Item">
+				<button id="searchSubmitButton">Search</button>
+			</div>
+			<!--Help Popup code-->
+		  <br>
+		 </div>
+		 
+	</div>
+	
+	<div id="map-container">
+		<div id="map-key">
+			<h3>Key by Price</h3>
+			<span class="key" > >= $0 <img src="http://labs.google.com/ridefinder/images/mm_20_white.png"></img></span>
+			<span class="key"> Low <img src="http://maps.google.com/mapfiles/ms/icons/green-dot.png"></img></span>
+			<span class="key"> Mid <img src="http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"></img></span>
+			<span class="key"> High <img src="http://maps.google.com/mapfiles/ms/icons/red-dot.png"></img></span>
+			<span class="key"> High High <img src="http://maps.google.com/mapfiles/ms/icons/dollar.png"></img></span>
+		</div>
+		<div id="map"></div>
+	</div>
+	
+	 
         <!--JS goes here-->
         <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC35EMzaJEToK-ZyOZ1SkBa1yKegKAfb8o&callback=initMap"></script>
 		<script>
