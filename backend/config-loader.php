c5952960d6eac358a7e6f285b826fa1668de5b31
benmucha <43706062+benmucha@users.noreply.github.com>
2023-03-30 18:17:35 -0400

    Amend GitIgnore Config

--- backend/config-loader.php
+++ backend/config-loader.php
@@ -1,11 +1,16 @@
 <?php
 
+if (!file_exists('../config.ini')){
+    echo 'ERROR: NO CONFIG FILE - Add a "config.ini" file to the root of this project.';
+    exit();
+}
+
 $config_ini = parse_ini_file('../config.ini');
 
 $python_path = $config_ini['python_path'];
 // This null check is fuzzy, and will say invalid if empty or doesn't exist:
 if ($python_path == NULL) {
-    echo 'CONFIG IS INVALID - Fix your "config.ini" file.';
+    echo 'ERROR: CONFIG IS INVALID - Fix your "config.ini" file.';
     exit();
 }
 
